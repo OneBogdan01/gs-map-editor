0f61bebdfc92a9dbc163bde17deee4e26e8a7345
Bogdan Mocanu <bogdan.game.development@gmail.com>
2025-10-07 17:22:51 +0200

    Format shaders with clang format

--- demo/shaders/generate_distance_field.glsl
+++ demo/shaders/generate_distance_field.glsl
@@ -7,10 +7,8 @@ layout(set = 0, binding = 0, r8) restrict uniform image2D edge_map;
 layout(set = 0, binding = 1, rg8) restrict uniform image2D buffer_a;
 layout(set = 0, binding = 2, rg8) restrict uniform image2D buffer_b;
 
-void main() {
-    // Get current pixel coordinates
-    ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
-    
-
-
+void main()
+{
+	// Get current pixel coordinates
+	ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
 }
--- demo/shaders/political_map.glsl
+++ demo/shaders/political_map.glsl
@@ -6,24 +6,22 @@ layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 // Input texture (read-only)
 layout(set = 0, binding = 0, rg8) restrict readonly uniform image2D color_lookup;
-layout(set = 0, binding = 1,rgba8) restrict readonly uniform image2D color_map;
+layout(set = 0, binding = 1, rgba8) restrict readonly uniform image2D color_map;
 
 layout(set = 0, binding = 2, rgba8) restrict writeonly uniform image2D political_map;
 
-void main() {
-    // Get current pixel coordinates
-    ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
-    
-    
-    // Read UVs from input texture
-    vec4 uv = imageLoad(color_lookup, coords);
-    // start from the first pixel
-    ivec2 lookup_uv = ivec2(uv.x  * 255, uv.y * 255);
-    // Get the color of that pixel from the normalized UVs
-    vec4 output_color = imageLoad(color_map, lookup_uv);
-    
-    
-    // Write to output texture
-    imageStore(political_map, coords, output_color);
+void main()
+{
+	// Get current pixel coordinates
+	ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
 
+	// Read UVs from input texture
+	vec4 uv = imageLoad(color_lookup, coords);
+	// start from the first pixel
+	ivec2 lookup_uv = ivec2(uv.x * 255, uv.y * 255);
+	// Get the color of that pixel from the normalized UVs
+	vec4 output_color = imageLoad(color_map, lookup_uv);
+
+	// Write to output texture
+	imageStore(political_map, coords, output_color);
 }
