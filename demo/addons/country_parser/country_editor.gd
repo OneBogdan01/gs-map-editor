fd99525543476cafb92816488e06d9ece6dd11dc
Bogdan Mocanu <bogdan.game.development@gmail.com>
2025-10-26 11:23:00 +0100

    Rename inspector files

--- demo/addons/country_parser/country_editor.gd
+++ demo/addons/country_parser/country_editor.gd
@@ -1,14 +1,13 @@
 @tool
 extends EditorPlugin
 
-var plugin
+var plugin: CountryInspector
 
 func _enter_tree():
 	plugin = preload("res://addons/country_parser/country_inspector.gd").new()
+
 	add_inspector_plugin(plugin)
 
 		
-	
-
 func _exit_tree():
 	remove_inspector_plugin(plugin)
